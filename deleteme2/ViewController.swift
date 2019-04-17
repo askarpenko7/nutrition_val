6101d04fcf097f2567997e07326431f280ac2e1b
alexander karpenko <alexander.karpenko@firstlinesoftware.com>
2019-04-17 11:37:54 +0300

    Initial Commit

---
+++ deleteme2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  deleteme2
+//
+//  Created by Alexander Karpenko on 17/04/2019.
+//  Copyright © 2019 Alexander Karpenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ deleteme2Tests/deleteme2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  deleteme2Tests.swift
+//  deleteme2Tests
+//
+//  Created by Alexander Karpenko on 17/04/2019.
+//  Copyright © 2019 Alexander Karpenko. All rights reserved.
+//
+
+import XCTest
+@testable import deleteme2
+
+class deleteme2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
